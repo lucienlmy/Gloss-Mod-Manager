1d83778831f233e426303ea001dd80bf96adb484
小莫 <xiaomo1433675239@outlook.com>
2023-04-25 11:32:58 +0800

    修复打包问题

--- src/stores/userMain.ts
+++ src/stores/userMain.ts
@@ -3,7 +3,7 @@ import { defineStore } from "pinia";
 export const userMain = defineStore('Main', {
     state: () => ({
         leftMenu: true,
-        leftMenuRail: true,
+        leftMenuRail: false,
     }),
     actions: {
 
