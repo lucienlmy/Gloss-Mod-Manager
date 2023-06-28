ba963f3a68a934c14b13df8e7098a02662b16e65
小莫 <xiaomo1433675239@outlook.com>
2023-06-28 15:46:45 +0800

    1.0.3 遗漏的一个文件

---
+++ src/components/Explore/Header.vue
@@ -0,0 +1,19 @@
+<script lang='ts' setup>
+import SelectGame from '@src/components/Manager/SelectGame.vue'
+
+
+</script>
+<template>
+    <v-row>
+        <v-col cols="12">
+            <SelectGame></SelectGame>
+        </v-col>
+    </v-row>
+</template>
+<script lang='ts'>
+
+export default {
+    name: 'ExploreHeader',
+}
+</script>
+<style lang='less' scoped></style>
